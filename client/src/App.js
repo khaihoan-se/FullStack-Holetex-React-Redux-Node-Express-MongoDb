f2e5f86cad84fbb766d87ef69a4a6cc8e15a18b7
khaihoan-se <khaihoan.softwareengineer@gmail.com>
2022-04-01 01:01:39 +0700

    add install redux-actions, err

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,12 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import * as actions from './redux/actions';
+
 function App() {
+  const dispatch = useDispatch()
+
+  dispatch(actions.getPosts.getPostsRequest)
+
   return (
     <div className="App">
       React Redux, node express mongodb
--- client/src/redux/sagas/index.js
+++ client/src/redux/sagas/index.js
@@ -1,5 +1,16 @@
-function* mySaga() {
 
+import { takeLatest, call } from 'redux-saga/effects';
+import * as actions from '../actions';
+import * as api from '../../api';
+
+
+function* fetchPostsSaga(action) {
+    const posts = yield call(api.fetchPosts);
+    console.log('[posts]', posts)
+};
+
+function* mySaga() {
+    yield takeLatest(actions.getPosts.getPostsRequest, fetchPostsSaga)
 };
 
 // generator function ES6
